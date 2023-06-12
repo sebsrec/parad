8a27b9025d349cd5b3242c79c2c7190b8d637858
sebsrec <77534400+sebsrec@users.noreply.github.com>
2023-06-12 10:37:06 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,109 +4,66 @@
 #include <chrono>
 #include <thread>
 #include "selectMonster.h"
-#include "skill.h"
-#include "skilltraining.h"
+
 
 int main() {
-    Monster* player1Monster = nullptr;
-    Monster* player2Monster = nullptr;
-
-    // Create some skill objects
-    Skill skill1("Fireball", 10, 0, 0, 5);
-    Skill skill2("Healing Touch", 0, 0, 20, 8);
-    Skill skill3("Shield Bash", 5, 15, 0, 10);
-
-    // Create a skill set object
-    SkillSet skillSet;
-
-    // Add skills to the set
-    skillSet.addSkill(skill1);
-    skillSet.addSkill(skill2);
-    skillSet.addSkill(skill3);
-
-    // Check if the skill set contains a specific skill
-    std::cout << "Skill Set contains Fireball: " << (skillSet.hasSkill("Fireball") ? "Yes" : "No") << std::endl;
-    std::cout << "Skill Set contains Ice Blast: " << (skillSet.hasSkill("Ice Blast") ? "Yes" : "No") << std::endl;
-
-    // Get all skills from the set and print their properties
-    const std::vector<Skill>& allSkills = skillSet.getAllSkills();
-    std::cout << "All Skills:" << std::endl;
-    for (const Skill& skill : allSkills) {
-        std::cout << "Name: " << skill.getName() << std::endl;
-        std::cout << "Attack Boost: " << skill.getAttackBoost() << std::endl;
-        std::cout << "Defense Boost: " << skill.getDefenseBoost() << std::endl;
-        std::cout << "Health Boost: " << skill.getHealthBoost() << std::endl;
-        std::cout << "Resource Cost: " << skill.getResourceCost() << std::endl;
-        std::cout << std::endl;
-    }
-
-    // Remove a skill from the set
-    skillSet.removeSkill("Healing Touch");
-
-    // Get all skills from the updated set and print them
-    std::cout << "Updated Skill Set:" << std::endl;
-    const std::vector<Skill>& updatedSkills = skillSet.getAllSkills();
-    for (const Skill& skill : updatedSkills) {
-        std::cout << "Name: " << skill.getName() << std::endl;
-        std::cout << "Attack Boost: " << skill.getAttackBoost() << std::endl;
-        std::cout << "Defense Boost: " << skill.getDefenseBoost() << std::endl;
-        std::cout << "Health Boost: " << skill.getHealthBoost() << std::endl;
-        std::cout << "Resource Cost: " << skill.getResourceCost() << std::endl;
-        std::cout << std::endl;
-    }
-
-    // Create trainer instances
+    //create trainer instances
     Trainer player1("Player 1");
     Trainer player2("Player 2");
 
-    // Create monster instances
-    auto* fireMonster = new FireMonster("Drogon", 5, 200, 30, 10);
-    auto* waterMonster = new WaterMonster("Aqua", 5, 200, 20, 20);
-    auto* grassMonster = new GrassMonster("Leafy", 5, 200, 25, 15);
-    auto* rockMonster = new RockMonster("Rocko", 5, 200, 25, 15);
-    auto* poisonMonster = new PoisonMonster("Venomancer", 5, 200, 25, 15);
+    //create monster instances
+    auto *fireMonster = new FireMonster("Drogon", 5, 150, 30, 5);
+    auto *waterMonster = new WaterMonster("Aqua", 5, 200, 20, 20);
+    auto *grassMonster = new GrassMonster("Leafy", 5, 200, 25, 15);
+    auto *rockMonster = new RockMonster("Rocko", 5, 200, 25, 15);
+    auto *poisonMonster = new PoisonMonster("Venomancer", 5, 100, 20, 15);
 
-    // Print initial health points of the monsters
-    std::cout << "\n" << player1Monster->getName() << "'s HP: " << player1Monster->getHealthPoints() << std::endl;
-    std::cout << player2Monster->getName() << "'s HP: " << player2Monster->getHealthPoints() << "\n" << std::endl;
+    //create item instances
+    //Item *itemP1 = new Item("Damage Potion (extra damage)", 20, 0, 0);
+    Item *itemP1 = new Item("Health Potion (extra health)", 0, 0, 50);
+    Item *itemP2 = new Item("Shield Potion (extra defense)", 0, 20, 0);
 
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    player1Monster->attack(player2Monster);
+    //add monsters to stl container
+    player1.addMonster(fireMonster, rockMonster, waterMonster, grassMonster, poisonMonster);
+    player2.addMonster(fireMonster, rockMonster, waterMonster, grassMonster, poisonMonster);
 
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    std::cout << "\n" << player1Monster->getName() << "'s HP: " << player1Monster->getHealthPoints() << std::endl;
-    std::cout << player2Monster->getName() << "'s HP: " << player2Monster->getHealthPoints() << "\n" << std::endl;
+    //select monsters to fight
+    Monster *player1Monster = selectMonster(player1);
+    Monster *player2Monster = selectMonster(player2);
 
-    // Add monsters to the trainers' containers
-    player1.addMonsters(fireMonster, rockMonster, waterMonster, grassMonster, poisonMonster, nullptr);
-    player2.addMonsters(fireMonster, rockMonster, waterMonster, grassMonster, poisonMonster, nullptr);
-
-    Item* itemP1 = nullptr;
-    Item* itemP2 = nullptr;
-
-    // Select monsters for battle
-    player1Monster = selectMonster(player1);
-    player2Monster = selectMonster(player2);
-
-    // Equip items to the monsters
+    //equip items to monsters
     player1Monster->equipItem(itemP1);
     player2Monster->equipItem(itemP2);
 
-    std::cout << "Player 1 has: " << itemP1->getName() << std::endl;
-    std::cout << "Player 2 has: " << itemP2->getName() << std::endl;
+    std::cout << "player1 has: " << itemP1->getName() << std::endl;
+    std::cout << "player2 has: " << itemP2->getName() << std::endl;
+    std::cout << "\nBATTLE BEGINS!" << std::endl;
 
-    // Continuously battle until one monster's health points reach zero
+    //fight until health reaches 0
     while (player1Monster->getHealthPoints() > 0 && player2Monster->getHealthPoints() > 0) {
         std::this_thread::sleep_for(std::chrono::seconds(1));
-        std::cout << "\n" << player1Monster->getName() << "'s HP: " << player1Monster->getHealthPoints() << std::endl;
-        std::cout << player2Monster->getName() << "'s HP: " << player2Monster->getHealthPoints() << "\n" << std::endl;
+        std::cout << "\n>> " << player1Monster->getName() << "'s HP: " << player1Monster->getHealthPoints() << std::endl;
+        std::cout << ">> " << player2Monster->getName() << "'s HP: " << player2Monster->getHealthPoints() << "\n" << std::endl;
 
         std::this_thread::sleep_for(std::chrono::seconds(1));
         player1Monster->attack(player2Monster);
 
         std::this_thread::sleep_for(std::chrono::seconds(1));
-        std::cout << "\n" << player1Monster->getName() << "'s HP: " << player1Monster->getHealthPoints() << std::endl;
-        std::cout << player2Monster->getName() << "'s HP: " << player2Monster->getHealthPoints() << "\n" << std::endl;
+        std::cout << "\n>> " << player1Monster->getName() << "'s HP: " << player1Monster->getHealthPoints() << std::endl;
+        std::cout << ">> " << player2Monster->getName() << "'s HP: " << player2Monster->getHealthPoints() << "\n" << std::endl;
+
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        player2Monster->attack(player1Monster);
+
+        if (player1Monster->getHealthPoints() <= 0 && player2Monster->getHealthPoints() != 0) {
+            std::cout << "\nPlayer 2 wins!" << std::endl;
+        }
+        if (player2Monster->getHealthPoints() <= 0 && player1Monster->getHealthPoints() != 0) {
+            std::cout << "\nPlayer 1 wins!" << std::endl;
+        }
+        if (player1Monster->getHealthPoints() == 0 && player2Monster->getHealthPoints() == 0) {
+            std::cout << "\nHurray both lose!" << std::endl;
+        }
     }
 
     player1.removeMonster();
